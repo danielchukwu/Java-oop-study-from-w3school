5ccc4452cbcfaaa7b3f819019b5b1dbda3310bdb
Daniel Chukwu <00danzy@gmail.com>
2022-07-01 07:54:25 +0100

    cmp 202 - Test Prep complete

---
+++ src/CMP202/JavaTestPrep.java
@@ -0,0 +1,21 @@
+package CMP202;
+
+// JAVA
+// Program - This is a java program that creates a method, a method that swaps two numbers and prints the result out
+public class JavaTestPrep {
+    public void swapTwoNumbers(int a, int b){
+        int x = a;
+        a = b;
+        b = x;
+
+        System.out.println("a: " + a + "| b: " + b);
+    }
+
+    public static void main (String [] args) {
+        // Object creation
+        // className variableName = new className();
+        JavaTestPrep swapNumbers = new JavaTestPrep();
+
+        swapNumbers.swapTwoNumbers(5, 10); // object calls method
+    }
+}
